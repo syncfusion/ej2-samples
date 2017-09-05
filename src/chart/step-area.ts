4d5b57e02df8c9b403381ae18831a4e1bbab915b
pipeline <pipeline@syncfusion.com>
2017-09-05 12:36:48 +0000

    config(EJ2-3292): Step area sample has been added.

---
+++ src/chart/step-area.ts
@@ -0,0 +1,56 @@
+import { Chart, StepAreaSeries, Legend, ILoadedEventArgs } from '@syncfusion/ej2-charts';
+Chart.Inject(StepAreaSeries, Legend);
+
+/**
+ * StepArea Series
+ */
+this.default = (): void => {
+    let chart: Chart = new Chart({
+
+        //Initializing Primary X Axis
+        primaryXAxis: {
+            title: 'Year',
+            lineStyle: { width: 0 },
+            valueType: 'Double',
+            edgeLabelPlacement: 'Shift'
+        },
+
+        //Initializing Primary Y Axis
+        primaryYAxis:
+        {
+            title: 'Production (billion kWh)',
+            valueType: 'Double',
+            labelFormat: '{value}B'
+        },
+
+        //Initializing Chart Series
+        series: [
+            {
+                type: 'StepArea',
+                dataSource: [{ x: 2000, y: 416 }, { x: 2001, y: 490 }, { x: 2002, y: 470 }, { x: 2003, y: 500 },
+                             { x: 2004, y: 449 }, { x: 2005, y: 470 }, { x: 2006, y: 437 }, { x: 2007, y: 458 },
+                             { x: 2008, y: 500 }, { x: 2009, y: 473 }, { x: 2010, y: 520 }, { x: 2011, y: 509 }],
+                name: 'Renewable',
+                xName: 'x', width: 2,
+                yName: 'y',
+            },
+            {
+                type: 'StepArea',
+                dataSource: [{ x: 2000, y: 180 }, { x: 2001, y: 240 }, { x: 2002, y: 370 }, { x: 2003, y: 200 },
+                             { x: 2004, y: 229 }, { x: 2005, y: 210 }, { x: 2006, y: 337 }, { x: 2007, y: 258 },
+                             { x: 2008, y: 300 }, { x: 2009, y: 173 }, { x: 2010, y: 220 }, { x: 2011, y: 309 }],
+                name: 'Non-Renewable',
+                xName: 'x', width: 2,
+                yName: 'y',
+            },
+        ],
+
+        //Initializing Chart title
+        title: 'Electricity- Production',
+        load: (args: ILoadedEventArgs) => {
+            let selectedTheme: string = location.hash.split('/')[1];
+            args.chart.theme = selectedTheme.indexOf('fabric') > -1 ? 'Fabric' : 'Material';
+        }
+    });
+    chart.appendTo('#container');
+};

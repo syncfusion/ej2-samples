3ca1b0c5cde11c6cfb268b92968229dd1e8af190
pipeline <pipeline@syncfusion.com>
2017-09-28 09:59:29 +0000

    Ej2 4244 form validator master

--- src/form-validator/default.ts
+++ src/form-validator/default.ts
@@ -1,40 +1,27 @@
-import { FormValidator, FormValidatorModel } from '@syncfusion/ej2-base';
+import { FormValidator, FormValidatorModel } from '@syncfusion/ej2-inputs';
 import { Button } from '@syncfusion/ej2-buttons';
 
 /**
  * FormValidator Sample
  */
 export default () => {
-    let customFn: (args: Object) => boolean = (args: {}) => {
-        return (args as { value: string[] }).value.length <= 5;
-    };
+
+    // Submit button
     let buttonFormValidate: Button = new Button();
     buttonFormValidate.appendTo('#validateSubmit');
+
+    // Reset button
     let buttonReset: Button = new Button();
     buttonReset.appendTo('#resetbtn');
+
+    //Custom placement 
     let options: FormValidatorModel = {
-        rules: {
-            Required: { required: true },
-            Email: { required: true, email: true },
-            Url: { url: true },
-            Date: { date: true },
-            DateISO: { dateIso: true },
-            Number: { number: true },
-            Digits: { digits: true },
-            MaxLength: { maxLength: 5 },
-            MinLength: { minLength: 5 },
-            RangeLength: { rangeLength: [5, 10] },
-            Range: { range: [5, 10] },
-            Max: { max: 5 },
-            Min: { min: 5 },
-            Regex: { regex: ['^[A-z]+$', 'Allowed only alphabets'] },
-            Custom: { custom: [customFn, 'Allowed char length is 5'] }
-        },
         customPlacement: (inputElement: HTMLElement, errorElement: HTMLElement) => {
             inputElement.parentElement.appendChild(errorElement);
         }
     };
 
+    //Form validation
     let formObj: FormValidator;
     formObj = new FormValidator('#formId', options);
 };

5eb13720de711d701864ed5b74999dbc85ad7542
pipeline <pipeline@syncfusion.com>
2017-10-06 17:39:25 +0000

    feature(EJ2-3507): Samples added for Grid to Excel,CSV & PDF export

---
+++ src/grid/exporting.ts
@@ -0,0 +1,40 @@
+import { Grid, Page, Toolbar, ExcelExport, PdfExport } from '@syncfusion/ej2-grids';
+import { orderData } from './datasource';
+
+Grid.Inject(Page, Toolbar, ExcelExport, PdfExport);
+/**
+ * Excel,PDF, CSV export sample
+ */
+this.default = (): void => {
+    let grid: Grid = new Grid(
+        {
+            dataSource: orderData.splice(0, 200),
+            allowExcelExport: true,
+            allowPdfExport: true,
+            allowPaging: true,
+            toolbar: ['excelexport', 'pdfexport', 'csvexport'],
+            pageSettings: { pageCount: 5 },
+            columns: [
+                { field: 'OrderID', headerText: 'Order ID', width: 120, textAlign: 'right' },
+                { field: 'CustomerName', headerText: 'Customer Name', width: 150 },
+                { field: 'OrderDate', headerText: 'Order Date', width: 130, format: 'yMd', textAlign: 'right' },
+                { field: 'Freight', width: 120, format: 'C2', textAlign: 'right' },
+                { field: 'ShipCountry', visible: false, headerText: 'Ship Country', width: 150 },
+                { field: 'ShipCity', visible: false, headerText: 'Ship City', width: 150 }
+            ]
+        });
+    grid.appendTo('#Grid');
+    grid.toolbarClick = (args: Object) => {
+        /* tslint:disable */
+        if ((args as any)['item'].id === 'Grid_pdfexport') {
+            grid.pdfExport();
+        }
+        if ((args as any)['item'].id === 'Grid_excelexport') {
+            grid.excelExport();
+        }
+        if ((args as any)['item'].id === 'Grid_csvexport') {
+            grid.csvExport();
+        }
+        /* tslint:enable */
+    };
+};
---
+++ src/grid/multiple-exporting.ts
@@ -0,0 +1,50 @@
+import { Grid, Toolbar, ExcelExport, PdfExport } from '@syncfusion/ej2-grids';
+import { orderData, productData } from './datasource';
+
+Grid.Inject(Toolbar, ExcelExport, PdfExport);
+/**
+ * Multiple Grid Exporting to Excel, PDF sample
+ */
+this.default = (): void => {
+    let firstGrid: Grid = new Grid({
+        dataSource: orderData.splice(0, 5),
+        allowExcelExport: true,
+        allowPdfExport: true,
+        toolbar: ['excelexport', 'pdfexport'],
+        columns: [
+            { field: 'OrderID', headerText: 'Order ID', textAlign: 'right', width: 120, type: 'number' },
+            { field: 'OrderDate', headerText: 'Order Date', textAlign: 'right', width: 140, format: 'yMd' },
+            { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
+            { field: 'Freight', headerText: 'Freight', textAlign: 'right', width: 120, format: 'C' },
+        ],
+    });
+
+    let secondGrid: Grid = new Grid({
+        dataSource: productData.splice(0, 5),
+        allowExcelExport: true,
+        allowPdfExport: true,
+        columns: [
+            { field: 'ProductID', headerText: 'Product ID', textAlign: 'right', width: 120 },
+            { field: 'ProductName', headerText: 'Product Name', width: 145 },
+            { field: 'UnitPrice', headerText: 'Unit Price', textAlign: 'right', width: 140, format: 'C2' },
+        ],
+    });
+    firstGrid.appendTo('#FirstGrid');
+    secondGrid.appendTo('#SecondGrid');
+    firstGrid.toolbarClick = (args: Object) => {
+        /* tslint:disable */
+        if ((args as any)['item'].id === 'FirstGrid_excelexport') {
+            let firstGridExcelExport: Promise<any> = firstGrid.excelExport({}, true);
+            firstGridExcelExport.then((bookData: any) => {
+                secondGrid.excelExport({}, false, bookData);
+            });
+        }
+        if ((args as any)['item'].id === 'FirstGrid_pdfexport') {
+            let firstGridPdfExport: Promise<Object> = firstGrid.pdfExport({}, true);
+            firstGridPdfExport.then((pdfData: Object) => {
+                secondGrid.pdfExport({}, false, pdfData);
+            });
+        }
+        /* tslint:enable */
+    };
+};

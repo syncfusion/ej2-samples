425d21215678cd2a4a1c231a79a04f8ae1e381c4
pipeline <pipeline@syncfusion.com>
2017-11-07 13:25:44 +0000

    bug(EJ2-3829): grid sample updated

--- src/grid/filtermenu.ts
+++ src/grid/filtermenu.ts
@@ -17,8 +17,7 @@ this.default = (): void => {
                 { field: 'OrderID', headerText: 'Order ID', width: 120, textAlign: 'right' },
                 { field: 'CustomerName', headerText: 'Customer Name', width: 150 },
                 { field: 'OrderDate', headerText: 'Order Date', width: 130, format: 'yMd', textAlign: 'right' },
-                { field: 'Freight', width: 120, format: 'C2', textAlign: 'right' },
-                { field: 'ShipCountry', allowFiltering: false, headerText: 'Ship Country', width: 150 }
+                { field: 'Freight', width: 120, format: 'C2', textAlign: 'right' }
             ],
             pageSettings: { pageCount: 5 }
         });
